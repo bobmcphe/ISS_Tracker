9abd4d7eafaa28675c9e5b2a87f6e7676bc0ee78
Ashley Casimir <ashcas@me.com>
2020-07-22 20:37:31 -0700

    created routes, completed / route and basic user route

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const morgan = require('morgan');
 const app = express();
 
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+const GEOCODE = process.env.GEOCODE;
+const NASA_KEY = process.env.NASA_KEY;
+const WEATHER = process.env.WEATHER;
 
 app.set('view engine', 'ejs');
 
@@ -26,16 +30,87 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(DATABASE_URL);
 
 // ----------------------------------------------
 // Route Definitions
 // ----------------------------------------------
 
-app.get('/', (req, res) => {
-    res.send('this is working');   
+app.get('/', handleHome); //landing page
+app.post('/basic', handleBasic); // basic user page
+app.get('/basic/results', handleResults); //displays results after 'get iss passes button'
+// app.get('/aboutus', handleAboutUs); //about us page
+// app.get('/info', handleInfo); //info page
+app.use('*', noFindHandler); // 404 route doesnt exist
+app.use(errorHandler); //errors
+
+
+// ----------------------------------------------
+// Route Functions
+// ----------------------------------------------
+
+function handleHome(req, res){
+    res.render('landingPage');
+}
+
+function handleBasic (req, res){
+    let SQL = `INSERT INTO savedTracker (name, city) VALUES ($1, $2) RETURNING *`; //sql command that saves persons username and city they entered on thelanding page
+
+    let values= [req.body.name, req.body.city]; // stores username and city values that were entered
+
+    client.query(SQL, values) //sends it to the DB
+        .then( results => {
+            console.log(results);
+        });
+}
+
+function Location (obj, query){
+    this.search_query = query;
+    this.formatted_query = obj.display_name;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
+}
+
+function handleResults (req, res){
+    const API_Geocode ='https://us1.locationiq.com/v1/search.php';
+
+    const cityParameters = {
+      key:GEOCODE,
+      q: city,
+      format: 'json',
+    };
+
+    superagent.get(API_Geocode)
+        .query(cityParameters)
+        .then( data => {
+            let cityData = new Location(data[0],city);
+        });
+
+    const API_issPasses = 'http://api.open-notify.org/iss-pass.json';
+
+    const API_issCurrentLocation = 'http://api.open-notify.org/iss-now.json';
+
+
+    superagent.get(API_issCurrentLocation)
+        .then(data => {
+            issPosition = {
+                lat: data.iss_position.latitude,
+                long: data.iss_position.longitude
+            }
+            
+        });
+}
+
+
+function noFindHandler(req, res){
+    res.status(404).send('Sorry, cannot find what you are looking for');
+}
+      
+
+function errorHandler (err, req, res){
+    res.status(500).send('Sorry, something went REALLY wrong', err);
+}
+
+client.connect(()=> {
+    app.listen(PORT,()=>console.log(`Server is running on Port: ${PORT}`));
 });
-
-
-app.listen(PORT, () => console.log(`Server is really really running on port ${PORT}`) );
-
