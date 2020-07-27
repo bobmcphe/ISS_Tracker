d274d65fd0f7c97bf32ab5efd43c80a59f488784
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-07-27 11:12:47 -0700

    some styling

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ function handleResults (req, res){
     // API query parameters
     // ----------------------------------------------
 
-    let date = new Date('2013-03-10T02:00:00Z');
+    let date = new Date();
     let dateOnly = date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate();  
     const cityParameters = {
         key:GEOCODE,
